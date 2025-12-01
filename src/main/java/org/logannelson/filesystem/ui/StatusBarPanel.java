2367de00810029692c3c4cdae32237b395c10a55
LTelson <gnelson908@gmail.com>
2025-12-01 13:39:07 -0500

    Added StatusBarPanel.java. At this point, a basic UI layout is visible. No functionality yet.

--- src/main/java/org/logannelson/filesystem/ui/StatusBarPanel.java
+++ src/main/java/org/logannelson/filesystem/ui/StatusBarPanel.java
@@ -1,4 +1,24 @@
 package org.logannelson.filesystem.ui;
 
-public class StatusBarPanel {
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class StatusBarPanel extends JPanel {
+
+    private final JLabel statusLabel;
+
+    public StatusBarPanel() {
+        super(new BorderLayout());
+
+        statusLabel = new JLabel("Ready");
+        setBorder(BorderFactory.createEtchedBorder());
+
+        add(statusLabel, BorderLayout.WEST);
+    }
+
+    public void setStatusMessage(String message) {
+        statusLabel.setText(message);
+    }
 }
